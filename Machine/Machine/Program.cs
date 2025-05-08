5d17d33deed88cd5847b11daacd5a8e12223da1c
NastyaZhilinaRis2 <149530615+NastyaZhilinaRis2@users.noreply.github.com>
2025-05-09 00:53:28 +0500

    доп

---
+++ Machine/Machine/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Machine
+{
+    internal class Program
+    {
+        static string InputAllowdStr(char[] allowedChars)
+        {
+            string strInput;
+            do
+            {
+                Console.WriteLine($"Введите строку (разрешенные символы: {string.Join(", ", allowedChars)}): ");
+                strInput = Console.ReadLine();
+
+            }
+            while (!strInput.All(c => allowedChars.Contains(c)));
+            return strInput;
+        }
+
+        static void Main(string[] args)
+        {
+            char[] x = { 'a', 'b', 'c', 'd'};
+            string str = InputAllowdStr(x);
+
+            int[,] transitionTable = new int[4, 4]
+                { 
+                    { 1, 3, 1, 3 }, 
+                    { 2, 2, 3, 3 },
+                    { 2, 2, 3, 3 },
+                    { 2, 2, 3, 3 }
+                };// столбцы - состояния, строки - входные данные
+            int state = 0;// состояния: 0 - начальное положение, 1 - гласная, 2 - согласная, 3 - ошибка (не перемежаются)
+            for (int i = 0; i < str.Length; i++)
+            {
+                int indexX = -1;
+                for (int j = 0; j < x.Length; j++)
+                {
+                    if (str[i] == ' ')
+                    {
+                        indexX = 4;
+                        break;
+                    }
+
+                    else if (str[i] == x[j])
+                    {
+                        indexX = j;
+                        break;
+                    }
+                }
+                state = transitionTable[indexX, state];
+            }
+            Console.WriteLine(state == 3 ? "Не перемежаются" : "Перемежаются");
+        }
+    }
+}
---
+++ Machine/tectMachine/Test1.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Machine.Tests
+{
+    public interface IInputReader
+    {
+        string ReadInput();
+    }
+
+    public class ConsoleInputReader : IInputReader
+    {
+        public string ReadInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+
+    public class Program
+    {
+        private readonly IInputReader _inputReader;
+
+        public Program(IInputReader inputReader)
+        {
+            _inputReader = inputReader;
+        }
+
+        public int CheckAlternation(string input)
+        {
+            // Логика проверки перемежающихся символов
+            if (string.IsNullOrEmpty(input))
+                return -1; // Или другое значение для пустой строки
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == input[i - 1])
+                    return 0; // Не перемежающиеся
+            }
+            return 1; // Перемежающиеся
+        }
+
+        public void Run()
+        {
+            string strInput = _inputReader.ReadInput();
+            int result = CheckAlternation(strInput);
+            Console.WriteLine(result);
+        }
+    }
+    public class MockInputReader : IInputReader
+    {
+        private readonly string _input;
+
+        public MockInputReader(string input)
+        {
+            _input = input;
+        }
+
+        public string ReadInput()
+        {
+            return _input;
+        }
+    }
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Test_AlternatingCharacters()
+        {
+            var mockInputReader = new MockInputReader("abab");
+            var program = new Program(mockInputReader);
+
+            int result = program.CheckAlternation("abab");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Test_NonAlternatingCharacters()
+        {
+            var mockInputReader = new MockInputReader("aa");
+            var program = new Program(mockInputReader);
+
+            int result = program.CheckAlternation("aa");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test_EmptyString()
+        {
+            var mockInputReader = new MockInputReader("");
+            var program = new Program(mockInputReader);
+
+            int result = program.CheckAlternation("");
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void Test_SingleCharacter()
+        {
+            var mockInputReader = new MockInputReader("a");
+            var program = new Program(mockInputReader);
+
+            int result = program.CheckAlternation("a");
+            Assert.AreEqual(1, result);
+        }
+    }
+}
